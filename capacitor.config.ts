1181e5ad6f28509fad6953fe177ebfc8ec116bc8
Vladimir Jovanovic <vjovanovic@showingtime.com>
2021-09-21 12:14:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-try',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
